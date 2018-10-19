ac00ee62c5260100cc7e0038a6b8ac05e8b68977
Renan Ribeiro <renan.ribeiro@dev11>
2018-10-19 14:37:32 -0300

    Finalizando countingsort e dando upload no radixsort

--- radixsort/radixsort.py
+++ radixsort/radixsort.py
@@ -33,19 +33,23 @@ def listAsc(size):
         listt.append(i+1)
     return listt
 
-#countingSort
-def sort(vetor, top):
-    n = len(vetor)
-    m = top + 1
-    aux = [0] * m               
-    for a in vetor:
-        aux[a] += 1             
-    i = 0
-    for a in range(m):            
-        for c in range(aux[a]): 
-            vetor[i] = a
-            i += 1
-    return vetor
+#radixSort
+def radixSort(lista):
+   
+    bucket = [ [],[],[],[],[],[],[],[],[],[] ]
+    maximo = max(lista)
+    div = 1
+    
+    while maximo > div:
+        for numList in lista:
+            bucket[int((numList/div)%10)].append(numList)
+
+        div = div * 10
+        lista = []
+        for i in range(10):
+            lista.extend(bucket[i])
+            bucket[i] = []
+
 
 def drawGraph(x,y,l,k,n, xl = "Nº de Elementos", yl = "Tempo(s)"):
     xnew = np.linspace(min(x), max(x), 10 * (max(x) - min(x)))
@@ -53,23 +57,22 @@ def drawGraph(x,y,l,k,n, xl = "Nº de Elementos", yl = "Tempo(s)"):
     suave = interpolate.BSpline(a, b, c, extrapolate=False)
     plt.subplot(n)
     plt.plot(xnew, suave(xnew), label="Curva Suave: "+k)
-    #plt.plot(x,y, label="Curva Sem Suaveização: "+k)
     plt.legend(bbox_to_anchor=(1, 1),bbox_transform=plt.gcf().transFigure)
     plt.ylabel(yl)
     plt.xlabel(xl)
     plt.title(l, fontsize=12)
 
 #template to sort list generated
-def midCase(nums0):
-    nums = nums0
-    time = []
-    for r in nums:
-        print(r)
-        vector = generateList(r)
-        tempo = timeit.timeit("sort({},{})".format(vector, r),setup="from __main__ import sort",number=1)
-        time.append(tempo)
+# def midCase(nums0):
+#     nums = nums0
+#     time = []
+#     for r in nums:
+#         print(r)
+#         vector = generateList(r)
+#         tempo = timeit.timeit("sort({})".format(vector),setup="from __main__ import sort",number=1)
+#         time.append(tempo)
 
-    drawGraph(nums, time,'Counting Sort', "Caso Medio",111, "Nº de Elementos", "Tempo(s)")
+#     drawGraph(nums, time,'Radix Sort', "Caso Medio",111, "Nº de Elementos", "Tempo(s)")
 
 def worseCase(nums1):
     nums=nums1
@@ -77,23 +80,23 @@ def worseCase(nums1):
     for r in nums:
         print (r)
         vector1 = listDesc(r)
-        tempo = timeit.timeit("sort({},{})".format(vector1, r),setup="from __main__ import sort",number=1)
+        tempo = timeit.timeit("sort({})".format(vector1),setup="from __main__ import sort",number=1)
         time1.append(tempo)
 
-    drawGraph(nums, time1, 'Counting Sort', "Pior Caso",111, "Nº de Elementos", "Tempo(s)")
+    drawGraph(nums, time1, 'Radix Sort', "Pior Caso",111, "Nº de Elementos", "Tempo(s)")
 
-# def bestCase(nums2):
-#     nums=nums2
-#     time2=[]
-#     for r in nums:
-#         print (r)
-#         vector1 = listAsc(r)
-#         tempo = timeit.timeit("sort({},{})".format(vector1, r),setup="from __main__ import sort",number=1)
-#         time2.append(tempo)
+def bestCase(nums2):
+    nums=nums2
+    time2=[]
+    for r in nums:
+        print (r)
+        vector1 = listAsc(r)
+        tempo = timeit.timeit("sort({})".format(vector1),setup="from __main__ import sort",number=1)
+        time2.append(tempo)
 
-#     drawGraph(nums, time2, 'Counting Sort', "Melhor Caso",111, "Nº de Elementos", "Tempo(s)")
+    drawGraph(nums, time2, 'Radix Sort', "Melhor Caso",111, "Nº de Elementos", "Tempo(s)")
 
-midCase(numbers)
+#midCase(numbers)
 worseCase(numbers)
-#bestCase(numbers)
+bestCase(numbers)
 plt.show()
